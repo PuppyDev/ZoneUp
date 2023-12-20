5f3404b3d8383a277f885374205c38d671ab9a25
Quốc Bảo <baodakmil123@gmail.com>
2023-12-20 21:42:55 +0700

    feat: Defind unplugin auto import

--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
+const WpAutoImport = require("unplugin-auto-import/webpack").default;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack(config) {
+        config.plugins.push(
+            WpAutoImport({
+                imports: [
+                    "react",
+                    {
+                        "next/navigation": [
+                            "useRouter",
+                            "useSearchParams",
+                            "useParams",
+                            "usePathname",
+                            "redirect",
+                            "permanentRedirect",
+                        ],
+                    },
+                    { "next/link": [["default", "Link"]] },
+                    { "next/image": [["default", "Image"]] },
+                    { "next/script": [["default", "Script"]] },
+                ],
+                dirs: ["./src/shared/components/ui", "./src/shared/utils"],
+            }),
+        );
+
+        return config;
+    },
+};
 
 module.exports = nextConfig;
