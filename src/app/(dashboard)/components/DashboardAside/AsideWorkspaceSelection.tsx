09b9cc11b9f7bb0fa65499c7055f02369b9b2fb0
Quốc Bảo <baodakmil123@gmail.com>
2023-12-26 22:35:57 +0700

    feat: update UI tickup more elegant

--- src/app/(dashboard)/components/DashboardAside/AsideWorkspaceSelection.tsx
+++ src/app/(dashboard)/components/DashboardAside/AsideWorkspaceSelection.tsx
@@ -1,5 +1,5 @@
 import { useOrganization } from "@clerk/nextjs";
-import { SidebarIcon } from "lucide-react";
+import { ChevronDownIcon, PlusIcon, SettingsIcon, SidebarIcon, Users2Icon } from "lucide-react";
 import Image from "next/image";
 
 const AsideWorkspaceSelection: React.FC = () => {
@@ -8,44 +8,75 @@ const AsideWorkspaceSelection: React.FC = () => {
     const { closeSidebarEvent } = usePreferencesState();
 
     return (
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between p-4">
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <Button variant="outline">
-                        <div className="flex items-center gap-1 select-none">
+                    <Button variant="ghost">
+                        <div className="flex items-center gap-1">
                             {organization?.imageUrl && (
                                 <Image
-                                    src={organization.imageUrl}
-                                    alt={organization.name}
                                     width={20}
                                     height={20}
-                                    priority
+                                    src={organization?.imageUrl}
+                                    alt={organization.name}
                                 />
                             )}
-
-                            <p>{organization?.name}</p>
+                            <p className="text-xl">{organization?.name}</p>
+                            <ChevronDownIcon size={20} />
                         </div>
                     </Button>
                 </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuContent className="p-4 w-72">
+                    <DropdownMenuLabel className="flex items-center gap-4 pt-0 font-normal">
+                        {organization?.imageUrl && (
+                            <Image
+                                width={40}
+                                height={40}
+                                src={organization?.imageUrl}
+                                alt={organization.name}
+                                className="rounded-xl"
+                            />
+                        )}
+                        <div>
+                            <p className="text-xl">{organization?.name}</p>
+                            <p className="text-sm">
+                                Free Forever * {organization?.membersCount} members
+                            </p>
+                        </div>
+                    </DropdownMenuLabel>
                     <DropdownMenuSeparator />
-                    <DropdownMenuItem>Profile</DropdownMenuItem>
-                    <DropdownMenuItem>Billing</DropdownMenuItem>
-                    <DropdownMenuItem>Team</DropdownMenuItem>
-                    <DropdownMenuItem>Subscription</DropdownMenuItem>
+                    <DropdownMenuGroup className="text-gray-300">
+                        <DropdownMenuItem className="gap-2 text-lg hover:cursor-pointer">
+                            <SettingsIcon size={20} />
+                            Settings
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="gap-2 text-lg hover:cursor-pointer">
+                            Upgrade
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="gap-2 text-lg hover:cursor-pointer">
+                            Apps
+                        </DropdownMenuItem>
+                        <DropdownMenuItem className="gap-2 text-lg hover:cursor-pointer">
+                            <Users2Icon size={20} />
+                            Manage users
+                        </DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem className="gap-2 text-lg hover:cursor-pointer">
+                        <PlusIcon />
+                        New Workspace
+                    </DropdownMenuItem>
                 </DropdownMenuContent>
             </DropdownMenu>
-
             <TooltipProvider>
                 <Tooltip>
                     <TooltipTrigger asChild>
-                        <Button variant="outline" size="sm" onClick={closeSidebarEvent}>
-                            <SidebarIcon />
+                        <Button variant="ghost" size="sm" className="" onClick={closeSidebarEvent}>
+                            <SidebarIcon size="20" />
                         </Button>
                     </TooltipTrigger>
                     <TooltipContent>
-                        <p> Toggle sidebar </p>
+                        <p>Collapse sidebar</p>
                     </TooltipContent>
                 </Tooltip>
             </TooltipProvider>
--- src/app/(dashboard)/components/DashboardMain.tsx
+++ src/app/(dashboard)/components/DashboardMain.tsx
@@ -1,11 +1,16 @@
 import { PropsWithChildren } from "react";
 import DashboardSidebar from "./DashboardSidebar";
+import MainTopBar from "./MainTopBar";
 
 const DashboardMain: React.FC<PropsWithChildren> = ({ children }) => {
     return (
-        <div className="flex w-full h-[calc(100%-72px)]">
+        <div className="flex w-full h-main-container-height">
             <DashboardSidebar />
-            <main className="w-full">{children}</main>
+            <main data-testid="top-bar-container" className="relative flex flex-col w-full h-full">
+                <MainTopBar />
+                <div className="main-background absolute top-0 left-0 w-full h-full bg-[url('/images/dashboard/background-dark.png')] bg-no-repeat bg-contain pointer-events-none" />
+                <div className="relative flex flex-col h-full p-4 overflow-y-auto">{children}</div>
+            </main>
         </div>
     );
 };
